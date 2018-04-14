ee3fbd5957613127204d77867b398c72021f9563
Yash Puthran <puthranyash1@gmail.com>
2018-04-15 00:30:55 +0530

    Added blog routes, auth

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+    googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+    mongoURI: "mongodb://user:user@ds117251.mlab.com:17251/node-starter",
+    cookieKey: '123123123'
+};
