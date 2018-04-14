2a78622a4416d71b837af280db7d16e3090535dc
Yash Puthran <puthranyash1@gmail.com>
2018-04-15 04:11:28 +0530

    Setting up server caching

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,35 +4,45 @@ const requireLogin = require('../middlewares/requireLogin');
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
+    app.get('/api/blogs/:id', requireLogin, async(req, res) => {
+        const blog = await Blog.findOne({
+            _user: req.user.id,
+            _id: req.params.id
+        });
+
+        res.send(blog);
     });
 
-    res.send(blog);
-  });
+    app.get('/api/blogs', requireLogin, async(req, res) => {
+        const redis = require('redis');
+        const redisUrl = 'redis://127.0.0.1:6379';
+        const client = redis.createClient(redisUrl);
+        const util = require('util');
+        client.get = util.promisify(client.get);
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+        //Lookup cache server first and return query
+        const cachedBlogs = await client.get(req.user.id);
 
-    res.send(blogs);
-  });
+        //Look up MongoDB incase query not in cache and update cache
+        const blogs = await Blog.find({ _user: req.user.id });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
-
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
+        res.send(blogs);
     });
 
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
+    app.post('/api/blogs', requireLogin, async(req, res) => {
+        const { title, content } = req.body;
+
+        const blog = new Blog({
+            title,
+            content,
+            _user: req.user.id
+        });
+
+        try {
+            await blog.save();
+            res.send(blog);
+        } catch (err) {
+            res.send(400, err);
+        }
+    });
 };
